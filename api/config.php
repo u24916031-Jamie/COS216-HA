448f457a0552bf3091fccfbbaf99317eda3064ee
IridescentDucky <bellaeng10@gmail.com>
2026-05-15 19:15:48 +0200

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,26 @@
+
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$db_server = "wheatley.cs.up.ac.za";
+$db_user   = $_ENV['DB_USER'];
+$db_pass   = $_ENV['DB_PASS'];
+$db_name   = $_ENV['DB_NAME'];
+// $db_server = "wheatley.cs.up.ac.za";
+// $db_user   = "u25090501";
+// $db_pass   = "6QA56TPMT4ACC5Z64LFB34P47ODBAASA";
+// $db_name   = "u25090501";
+// $conn = "";
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass , $db_name);
+
+if (!$conn) {
+    die("Connection failed: " .
+        mysqli_connect_error());
+}
+
+?>
